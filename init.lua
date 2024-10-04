f03659ef6e3ca6280dbd7242e02102dbb1b11bc9
Gleb.Chashchukhin <Gleb.Chashchukhin@esystem-mtg.de>
2024-10-04 10:21:24 +0200

    added autocmd to remove trailing spaces on buffer write

--- init.lua
+++ init.lua
@@ -204,6 +204,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  pattern = { '*' },
+  callback = function()
+    local save_cursor = vim.fn.getpos '.'
+    vim.cmd [[%s/\s\+$//e]]
+    vim.fn.setpos('.', save_cursor)
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
