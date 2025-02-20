372bbc233cf2a8d19a4f82e4dc5dcf96007b6055
Gleb.Chashchukhin <Gleb.Chashchukhin@esystem-mtg.de>
2025-02-20 11:08:54 +0100

    added a shortcut for searching changed files

--- init.lua
+++ init.lua
@@ -425,6 +425,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', '<leader>sc', builtin.git_status, { desc = '[S]earch [C]hanged' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>st', function()
